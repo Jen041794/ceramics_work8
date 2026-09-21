7d7ba9d750af047cd57d7369fcfefe11cbc3a6a6
Jen940717 <feijia0724417@gmail.com>
2026-09-21 20:22:29 +0800

    修正 GitHub Pages 部署路徑設定
    vite base path 改成對應新倉庫名稱 ceramics_work8

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/reactweek-classproject8-main/',
+  base: '/ceramics_work8/',
   plugins: [react()],
 })
